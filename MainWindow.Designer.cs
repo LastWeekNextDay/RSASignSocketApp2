f6c76e57ed9f39d788f5b6f6f880d2aaa14b1b3d
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-04-26 14:52:17 +0300

    final?

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -32,18 +32,18 @@
             this.publicKeyLabel = new System.Windows.Forms.Label();
             this.nLabel = new System.Windows.Forms.Label();
             this.eLabel = new System.Windows.Forms.Label();
-            this.nTextBox = new System.Windows.Forms.TextBox();
-            this.eTextBox = new System.Windows.Forms.TextBox();
             this.digitalSignLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.messageLabel = new System.Windows.Forms.Label();
             this.messageTextBox = new System.Windows.Forms.RichTextBox();
             this.sendButton = new System.Windows.Forms.Button();
+            this.nTextBox = new System.Windows.Forms.RichTextBox();
+            this.eTextBox = new System.Windows.Forms.RichTextBox();
+            this.signatureTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // publicKeyLabel
             // 
-            this.publicKeyLabel.Location = new System.Drawing.Point(33, 29);
+            this.publicKeyLabel.Location = new System.Drawing.Point(33, 9);
             this.publicKeyLabel.Name = "publicKeyLabel";
             this.publicKeyLabel.Size = new System.Drawing.Size(113, 23);
             this.publicKeyLabel.TabIndex = 0;
@@ -51,7 +51,7 @@
             // 
             // nLabel
             // 
-            this.nLabel.Location = new System.Drawing.Point(12, 52);
+            this.nLabel.Location = new System.Drawing.Point(12, 32);
             this.nLabel.Name = "nLabel";
             this.nLabel.Size = new System.Drawing.Size(45, 23);
             this.nLabel.TabIndex = 1;
@@ -59,44 +59,23 @@
             // 
             // eLabel
             // 
-            this.eLabel.Location = new System.Drawing.Point(12, 89);
+            this.eLabel.Location = new System.Drawing.Point(12, 114);
             this.eLabel.Name = "eLabel";
             this.eLabel.Size = new System.Drawing.Size(100, 23);
             this.eLabel.TabIndex = 2;
             this.eLabel.Text = "e";
             // 
-            // nTextBox
-            // 
-            this.nTextBox.Location = new System.Drawing.Point(33, 53);
-            this.nTextBox.Name = "nTextBox";
-            this.nTextBox.Size = new System.Drawing.Size(230, 22);
-            this.nTextBox.TabIndex = 3;
-            // 
-            // eTextBox
-            // 
-            this.eTextBox.Location = new System.Drawing.Point(33, 89);
-            this.eTextBox.Name = "eTextBox";
-            this.eTextBox.Size = new System.Drawing.Size(230, 22);
-            this.eTextBox.TabIndex = 4;
-            // 
             // digitalSignLabel
             // 
-            this.digitalSignLabel.Location = new System.Drawing.Point(33, 149);
+            this.digitalSignLabel.Location = new System.Drawing.Point(33, 179);
             this.digitalSignLabel.Name = "digitalSignLabel";
             this.digitalSignLabel.Size = new System.Drawing.Size(113, 23);
             this.digitalSignLabel.TabIndex = 5;
             this.digitalSignLabel.Text = "Digital Signature";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(33, 175);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 22);
-            this.textBox1.TabIndex = 6;
-            // 
             // messageLabel
             // 
-            this.messageLabel.Location = new System.Drawing.Point(33, 244);
+            this.messageLabel.Location = new System.Drawing.Point(33, 351);
             this.messageLabel.Name = "messageLabel";
             this.messageLabel.Size = new System.Drawing.Size(100, 23);
             this.messageLabel.TabIndex = 7;
@@ -104,7 +83,7 @@
             // 
             // messageTextBox
             // 
-            this.messageTextBox.Location = new System.Drawing.Point(33, 270);
+            this.messageTextBox.Location = new System.Drawing.Point(33, 377);
             this.messageTextBox.Name = "messageTextBox";
             this.messageTextBox.Size = new System.Drawing.Size(230, 118);
             this.messageTextBox.TabIndex = 8;
@@ -112,33 +91,59 @@
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(33, 406);
+            this.sendButton.Location = new System.Drawing.Point(33, 512);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(230, 32);
             this.sendButton.TabIndex = 9;
             this.sendButton.Text = "Send";
             this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(33, 35);
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(230, 56);
+            this.nTextBox.TabIndex = 10;
+            this.nTextBox.Text = "";
+            // 
+            // eTextBox
+            // 
+            this.eTextBox.Location = new System.Drawing.Point(33, 114);
+            this.eTextBox.Name = "eTextBox";
+            this.eTextBox.Size = new System.Drawing.Size(230, 51);
+            this.eTextBox.TabIndex = 11;
+            this.eTextBox.Text = "";
+            // 
+            // signatureTextBox
+            // 
+            this.signatureTextBox.Location = new System.Drawing.Point(33, 205);
+            this.signatureTextBox.Name = "signatureTextBox";
+            this.signatureTextBox.Size = new System.Drawing.Size(230, 132);
+            this.signatureTextBox.TabIndex = 12;
+            this.signatureTextBox.Text = "";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 450);
+            this.ClientSize = new System.Drawing.Size(307, 555);
+            this.Controls.Add(this.signatureTextBox);
+            this.Controls.Add(this.eTextBox);
+            this.Controls.Add(this.nTextBox);
             this.Controls.Add(this.sendButton);
             this.Controls.Add(this.messageTextBox);
             this.Controls.Add(this.messageLabel);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.digitalSignLabel);
-            this.Controls.Add(this.eTextBox);
-            this.Controls.Add(this.nTextBox);
             this.Controls.Add(this.eLabel);
             this.Controls.Add(this.nLabel);
             this.Controls.Add(this.publicKeyLabel);
             this.Name = "MainWindow";
             this.Text = "Application2";
             this.ResumeLayout(false);
-            this.PerformLayout();
         }
+
+        
 
         private System.Windows.Forms.Button sendButton;
 
@@ -146,10 +151,10 @@
         private System.Windows.Forms.RichTextBox messageTextBox;
 
         private System.Windows.Forms.Label digitalSignLabel;
-        private System.Windows.Forms.TextBox textBox1;
-
-        private System.Windows.Forms.TextBox nTextBox;
-        private System.Windows.Forms.TextBox eTextBox;
+        private System.Windows.Forms.RichTextBox signatureTextBox;
+        
+        private System.Windows.Forms.RichTextBox nTextBox;
+        private System.Windows.Forms.RichTextBox eTextBox;
 
         private System.Windows.Forms.Label nLabel;
         private System.Windows.Forms.Label eLabel;
--- MainWindow.cs
+++ MainWindow.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,60 @@ namespace RSASignatureApplication2
 {
     public partial class MainWindow : Form
     {
+        private readonly SocketInteractor _socketInteractor;
+        
         public MainWindow()
         {
+            _socketInteractor = new SocketInteractor();
+            _socketInteractor.Connect();
+            var socketT = new Thread(async () =>
+            {
+                while (true)
+                {
+                    await _socketInteractor.Listen();
+                }
+            });
+            var t = new Thread(new ThreadStart(CheckChangeLoop));
+            t.Start();
+            socketT.Start();
             InitializeComponent();
+        }
+
+        private void InvokeUI(Action action)
+        {
+            BeginInvoke(new MethodInvoker(action));
+        }
+
+        private void CheckChangeLoop()
+        {
+            while (true)
+            {
+                if (_socketInteractor.Change != 1) continue;
+                _socketInteractor.Change = 0;
+                InvokeUI(() =>
+                {
+                    var dialogResult = MessageBox.Show($"New message incoming. Do you wish to accept?", 
+                        "Incoming.", MessageBoxButtons.YesNo);
+                    if (dialogResult != DialogResult.Yes) return;
+                    var data = _socketInteractor.Data.Split(new string[] { "~|~" }, StringSplitOptions.None);
+                    var message = data[1];
+                    var signature = data[2];
+                    var modulus = data[3];
+                    var exponent = data[4];
+
+                    messageTextBox.Text = message;
+                    signatureTextBox.Text = signature;
+                    nTextBox.Text = modulus;
+                    eTextBox.Text = exponent;
+                });
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            var message = "3~|~" + messageTextBox.Text + "~|~" + signatureTextBox.Text + "~|~" + nTextBox.Text + "~|~" +
+                          eTextBox.Text;
+            _socketInteractor.Send(message);
         }
     }
 }
